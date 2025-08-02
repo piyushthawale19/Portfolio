429a2aab582e070bf976e3b698815a475facbc1f
piyushthawale <piyushthawale19@gmail.com>
2025-08-03 02:46:53 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import {NotFound} from './pages/NotFound'
-import {Toaster} from './components/UI/toaster'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
-
-
-
   return (
     <>
       <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route  path="*"  element={<NotFound/>} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
